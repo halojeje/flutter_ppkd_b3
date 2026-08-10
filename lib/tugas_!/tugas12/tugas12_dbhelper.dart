48335f5a9b13dccd7f8345acd76bd30f69bbb703
Jeihan <jeihan.azib@gmail.com>
2026-08-10 09:17:19 +0700

    Implement DBHelper for user management with SQLite

---
+++ lib/tugas_!/tugas12/tugas12_dbhelper.dart
@@ -0,0 +1,98 @@
+import 'package:flutter_ppkd_b3/tugas_!/tugas12_usermodel12.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+
+  factory DBHelper() {
+    return _instance;
+  }
+
+  DBHelper._internal();
+
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await _initDatabase();
+
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasePath = await getDatabasesPath();
+
+    final pathDatabase = join(databasePath, 'turkish_academy.db');
+
+    return await openDatabase(pathDatabase, version: 1, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nama TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+      )
+    ''');
+  }
+
+  Future<bool> registerUser(UserModelSQL user) async {
+    try {
+      final db = await database;
+
+      await db.insert(
+        'users',
+        user.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.abort,
+      );
+
+      return true;
+    } catch (e) {
+      print('Error register user: $e');
+
+      return false;
+    }
+  }
+
+  Future<List<UserModelSQL>> getUsers() async {
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query(
+      'users',
+      orderBy: 'id DESC',
+    );
+
+    return List.generate(maps.length, (index) {
+      return UserModelSQL.fromMap(maps[index]);
+    });
+  }
+
+  Future<UserModelSQL?> loginUser(String email, String password) async {
+    final db = await database;
+
+    final result = await db.query(
+      'users',
+      where: 'email = ? AND password = ?',
+      whereArgs: [email, password],
+      limit: 1,
+    );
+
+    if (result.isNotEmpty) {
+      return UserModelSQL.fromMap(result.first);
+    }
+
+    return null;
+  }
+
+  Future<int> deleteUser(int id) async {
+    final db = await database;
+
+    return await db.delete('users', where: 'id = ?', whereArgs: [id]);
+  }
+}
