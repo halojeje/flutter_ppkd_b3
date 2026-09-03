7948255facbf47ec757f7dbd13f2a6924df14ae5
Jeihan <jeihan.azib@gmail.com>
2026-09-03 08:40:01 +0700

    Update api_service.dart

--- lib/tugas_!/tugas15/services/api_service.dart
+++ lib/tugas_!/tugas15/services/api_service.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_ppkd_b3/day_32/models/movie_model.dart'; // sesuaikan path
+import 'package:flutter_ppkd_b3/tugas_!/tugas15_API/model/movie_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 // File generasi otomatis yang dibuat oleh build_runner (retrofit_generator)
 part 'api_service.g.dart';
 
-@RestApi(baseUrl: 'https://ghibliapi.dev')
+@RestApi(baseUrl: 'https://ghibliapi.vercel.app')
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
