d6e641001a54e7739578dcdf0979c449f5db9eeb
Jeihan <jeihan.azib@gmail.com>
2026-09-03 08:40:13 +0700

    Update api_service.g.dart

--- lib/tugas_!/tugas15/services/api_service.g.dart
+++ lib/tugas_!/tugas15/services/api_service.g.dart
@@ -12,7 +12,7 @@ part of 'api_service.dart';
 
 class _ApiService implements ApiService {
   _ApiService(this._dio, {this.baseUrl, this.errorLogger}) {
-    baseUrl ??= 'https://ghibliapi.dev';
+    baseUrl ??= 'https://ghibliapi.vercel.app';
   }
 
   final Dio _dio;
